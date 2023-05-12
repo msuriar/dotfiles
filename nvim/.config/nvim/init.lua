254dda5030a0903b88624b9313024cad5bdd44a3
Murali Suriar <code@suriar.com>
2023-05-12 22:36:04 +0100

    Add more section headings and hierarchy.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,10 @@
--- Appearance
+-- Options
+--- Appearance
 vim.opt.list = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Completion
+--- Completion
 vim.opt.wildmode = "longest:full"
 
 --flag problematic whitespace (trailing and spaces before tabs)
@@ -12,30 +13,30 @@ vim.opt.wildmode = "longest:full"
 vim.cmd "highlight RedundantSpaces term=standout ctermbg=red guibg=red"
 vim.cmd [[match RedundantSpaces /\s\+$\| \+\ze\t/]] -- \ze sets end of match so only spaces highlighted
 
--- Scrolling
+--- Scrolling
 vim.opt.scrolloff = 20
 
--- Search
+--- Search
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Mappings
--- convenience alias
+--- convenience alias
 local keymap = vim.api.nvim_set_keymap
 
--- Remap space as leader key
+--- Remap space as leader key
 keymap("", "<Space>", "<Nop>", {})
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Substitute word under cursor
+--- Substitute word under cursor
 keymap("n", "<leader>s", [[:%s/\<<C-R><C-W>\>/]], {})
 
--- toggle highlighting search pattern with carriage return
+--- toggle highlighting search pattern with carriage return
 keymap("n", "<CR>", ":setlocal hlsearch! hlsearch? <CR>", { noremap = true, silent = true })
 
--- Splits
+--- Splits
 keymap("n", [[<leader>|]], ":vsplit<CR>", {})
 
 keymap("n", "<leader>_", [[:split<CR>]], {})
@@ -43,14 +44,14 @@ keymap("n", "<leader>_", [[:split<CR>]], {})
 keymap("n", "<leader>o", [[:only<CR>]], {})
 keymap("n", "<leader>q", [[:close<CR>]], {})
 
--- Move between splits easily
+--- Move between splits easily
 keymap("n", "<C-h>", [[<C-w>h]], {})
 keymap("n", "<C-j>", [[<C-w>j]], {})
 keymap("n", "<C-k>", [[<C-w>k]], {})
 keymap("n", "<C-l>", [[<C-w>l]], {})
 
 
--- Rotate between various combinations of number/relativenumber settings
+--- Rotate between various combinations of number/relativenumber settings
 local function numbertoggle()
 	if (vim.o.relativenumber and vim.o.number)
 		then
@@ -64,5 +65,5 @@ local function numbertoggle()
 	end
 end
 
--- ... and bind to <leader>n
+--- ... and bind to <leader>n
 vim.keymap.set("n", "<leader>n", numbertoggle, {})
