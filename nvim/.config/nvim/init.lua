6c30443d04b3f4e1f285ba645a47bbb16d43ee41
Murali Suriar <code@suriar.com>
2023-05-13 22:42:55 +0100

    Skeleton completion copied from fatih.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,11 +27,62 @@ require("lazy").setup({
 		config = function()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+			luasnip.config.setup {}
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 			cmp.setup({
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
 					end,
+				},
+				mapping = cmp.mapping.preset.insert {
+					['<C-n>'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
+					['<C-d>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<CR>'] = cmp.mapping.confirm { select = true },
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						elseif cmp.visible() then
+							cmp.select_next_item()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+				},
+				preselect = cmp.PreselectMode.None,
+				window = {
+					documentation = cmp.config.window.bordered(),
+				},
+				view = {
+					entries = {
+						name = "custom",
+						selection_order = "near_cursor",
+					},
+				},
+				confirm_opts = {
+					behavior = cmp.ConfirmBehavior.Insert,
+				},
+				sources = {
+					{ name = 'nvim_lsp' },
+					{ name = "luasnip", keyword_length = 2},
+					{ name = "buffer", keyword_length = 5},
 				},
 			})
 		end
