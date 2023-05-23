36f3103ed8100129286833c3d4847a9dec2f9373
Murali Suriar <code@suriar.com>
2023-05-24 00:43:21 +0100

    Add LSP config.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -94,9 +94,41 @@ require("lazy").setup({
       "nvim-lua/plenary.nvim",
     }
   },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
 })
 -- End plugins
 
+-- LSP setup
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
 
 
 vim.cmd.colorscheme "catppuccin-frappe"
