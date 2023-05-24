2b0c1b7d8deae51314f4661cb6986e5882ac17af
Murali Suriar <code@suriar.com>
2023-05-24 22:59:10 +0100

    Add vim-go

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -134,7 +134,21 @@ require("lazy").setup({
         }
       )
     end
-  }
+  },
+  {
+    'fatih/vim-go',
+    config = function ()
+      vim.g['go_gopls_enabled'] = 0
+      vim.g['go_code_completion_enabled'] = 0
+      vim.g['go_fmt_autosave'] = 0
+      vim.g['go_imports_autosave'] = 0
+      vim.g['go_mod_fmt_autosave'] = 0
+      vim.g['go_doc_keywordprg_enabled'] = 0
+      vim.g['go_def_mapping_enabled'] = 0
+      vim.g['go_textobj_enabled'] = 0
+      vim.g['go_list_type'] = 'quickfix'
+    end
+  },
 })
 -- End plugins
 
