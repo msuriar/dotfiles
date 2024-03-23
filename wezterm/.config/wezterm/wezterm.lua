f2c63e6eece6532e36e038f09cf465c1b833cba9
Murali Suriar <code@suriar.com>
2024-03-23 23:13:07 +0000

    Update wezterm - disable mouse scroll.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,28 +1,39 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder{}
+	config = wezterm.config_builder({})
 end
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = "Catppuccin Macchiato"
 
-config.default_cursor_style = 'BlinkingUnderline'
+config.default_cursor_style = "BlinkingUnderline"
 
 config.font_size = 14.0
 
 config.keys = {
-  {
-    key = 'n',
-    mods = 'CMD',
-    action = wezterm.action.DisableDefaultAssignment,
-  },
-  {
-    key = 't',
-    mods = 'CMD',
-    action = wezterm.action.DisableDefaultAssignment,
-  },
+	{
+		key = "n",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "t",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+		action = wezterm.action.Nop,
+	},
+	{
+		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+		action = wezterm.action.Nop,
+	},
 }
 
 config.hide_tab_bar_if_only_one_tab = true
